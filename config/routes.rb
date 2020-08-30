97dd5498def1d0ca8a384f0bd1d3dc996824dbde
taimushimizu <213.f.r48132@gmail.com>
2020-08-31 00:22:02 +0900

    password_resetsリソースの追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get'/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
